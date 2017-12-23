10046d4138e4f07ba77683ee5d12187fb934cab0
MaxGsomGsom <max_kuzmin@outlook.com>
2017-12-23 19:40:43 +0300

    Ex2 70% done

--- DSS_Ex2/include/raid6.cpp
+++ DSS_Ex2/include/raid6.cpp
@@ -1,17 +1,19 @@
 #include <galois.cpp>
 
+
 int Calculate_S0S1(byte *D, int nDrives, int nSyndromes) {
 
-    if (nDrives<1 || nSyndromes!=2) return -1;
+    if (D == nullptr || nDrives<1 || nSyndromes!=2) return -1;
 
-    //calc first
+    //Calc first syndrome
     byte S0 = 0;
     for (int i=0; i<nDrives; i++) {
         S0 = GF_sum(S0, D[i]);
     }
     D[nDrives] = S0;
 
-    //calc second
+
+    //Calc second syndrome
     byte S1 = 0;
     for (int i=0; i<nDrives; i++) {
         //S1+=D[i]*x^i
@@ -20,19 +22,19 @@ int Calculate_S0S1(byte *D, int nDrives, int nSyndromes) {
              GF_pow(_primElem, i)));
     }
     D[nDrives+1] = S1;
-
-    return 0;
 }
 
 
 int Recovery1d_S0(byte *D, int nDrives, int nSyndromes, int *Failed) {
 
-    if (nDrives<1 || nSyndromes!=2 || Failed[0] < 0 || Failed[0] >= nDrives) return -1;
+    if (D == nullptr || nDrives<1 || nSyndromes!=2 ||
+            Failed[0] < 0 || Failed[0] >= nDrives) return -1;
 
-    //Sum all data (except failed block) and syndrome 0
+    D[Failed[0]] = 0b00;
+
+    //Sum all data (except failed block, cause it's 0) and syndrome 0
     byte dRecovered = 0;
     for (int i=0; i<(nDrives+1); i++) {
-        if (i!=Failed[0])
             dRecovered = GF_sum(dRecovered, D[i]);
     }
     D[Failed[0]] = dRecovered;
@@ -43,11 +45,12 @@ int Recovery1d_S0(byte *D, int nDrives, int nSyndromes, int *Failed) {
 
 int Recovery1d_S1(byte *D, int nDrives, int nSyndromes, int *Failed) {
 
-    if (nDrives<1 || nSyndromes!=2 || Failed[0] < 0 || Failed[0] >= nDrives) return -1;
+    if (D == nullptr || nDrives<1 || nSyndromes!=2 ||
+            Failed[0] < 0 || Failed[0] >= nDrives) return -1;
 
     D[Failed[0]] = 0b00;
 
-    //calc second syndrome with failed block equals 0
+    //Calc second syndrome with failed block equals 0
     byte S1d = 0;
     for (int i=0; i<nDrives; i++) {
         //S1d+=D[i]*x^i
@@ -56,12 +59,72 @@ int Recovery1d_S1(byte *D, int nDrives, int nSyndromes, int *Failed) {
              GF_pow(_primElem, i)));
     }
 
-    //calc failed block
-    //dFailed = (S1+S1d)/x^dFiledPos
-    D[Failed[0]] = GF_div(
-                   GF_sum(D[nDrives+1], S1d),
-                   GF_pow(_primElem, Failed[0]));
+    //Calc failed block
+    //dFailed = (S1+S1d)*x^(-dFiledPos)
+    D[Failed[0]] = GF_mul(
+                     GF_sum(D[nDrives+1], S1d),
+                     GF_pow(
+                       GF_inv(_primElem),
+                       Failed[0]));
 
     return 0;
 }
 
+
+int Recovery2d_S0S1(byte *D, int nDrives, int nSyndromes, int *Failed) {
+
+    if (D == nullptr || nDrives<1 || nSyndromes!=2 ||
+            Failed[0] < 0 || Failed[0] >= nDrives ||
+            Failed[1] < 0 || Failed[1] >= nDrives ||
+            Failed[0] >= Failed[1]) //Index of first failed element must be less then second
+        return -1;
+
+
+    D[Failed[0]] = 0b00;
+    D[Failed[1]] = 0b00;
+
+    //Calc first syndrome with failed blocks equals 0
+    byte S0dd = 0;
+    for (int i=0; i<nDrives; i++) {
+        S0dd = GF_sum(S0dd, D[i]);
+    }
+
+    //Calc second syndrome with failed blocks equals 0
+    byte S1dd = 0;
+    for (int i=0; i<nDrives; i++) {
+        //S1dd+=D[i]*x^i
+        S1dd = GF_sum(S1dd,
+               GF_mul(D[i],
+               GF_pow(_primElem, i)));
+    }
+
+    //Calc failed block 1
+    // primElemInvPow = x^(d1FiledPos-d0FiledPos)
+    byte primElemInvPow = GF_pow(
+                             _primElem,
+                             Failed[1]-Failed[0]);
+
+    // sumS0S0dd = S0+S0dd
+    byte sumS0S0dd = GF_sum(D[nDrives], S0dd);
+
+    // d0Failed = (S1+S1dd)*x^(-d0FiledPos) + (S0+S0dd)*x^(d1FiledPos-d0FiledPos) /
+    // (x^(d1FiledPos-d0FiledPos) + 1)
+    D[Failed[0]] = GF_div(
+                     GF_sum(
+                       GF_mul(
+                         GF_sum(D[nDrives+1], S1dd),
+                         GF_pow(
+                           GF_inv(_primElem),
+                           Failed[0])),
+                       GF_mul(
+                         sumS0S0dd,
+                         primElemInvPow)),
+                     GF_sum(primElemInvPow, 1));
+
+
+    //Calc failed block 2
+    // d1Failed = (S0+S0dd)+d0;
+    D[Failed[1]] = GF_sum(sumS0S0dd, D[Failed[0]]);
+
+    return 0;
+}
--- DSS_Ex2/main.cpp
+++ DSS_Ex2/main.cpp
@@ -7,22 +7,29 @@ int main()
 {
     int nDrives = 14, nSyndromes = 2;
     byte D[16] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x1b};
+    int dFailedPos[2] = {2, 6};
 
-    Calculate_S0S1(&(D[0]), nDrives, nSyndromes);
-    byte S0=D[14], S1 = D[15];
-    bool test1 = S0 == 0xff && S1 == 0x1b;
+    //Calc syndromes
+    Calculate_S0S1(&D[0], nDrives, nSyndromes);
+    bool test1 = D[14] == 0xff && D[15] == 0x1b;
 
-    int dnFailed = 2;
-    Recovery1d_S0(&(D[0]), nDrives, nSyndromes, &dnFailed);
-    bool test2 = D[dnFailed] == 0x33;
+    //Recovery 1 block with S0
+    Recovery1d_S0(&D[0], nDrives, nSyndromes, &dFailedPos[0]);
+    bool test2 = D[dFailedPos[0]] == 0x33;
 
-    Recovery1d_S1(&(D[0]), nDrives, nSyndromes, &dnFailed);
-    bool test3 = D[dnFailed] == 0x33;
+    //Recovery 2 block with S0
+    Recovery1d_S1(&D[0], nDrives, nSyndromes, &dFailedPos[0]);
+    bool test3 = D[dFailedPos[0]] == 0x33;
+
+    //Recovery 2 blocks
+    Recovery2d_S0S1(&D[0], nDrives, nSyndromes, &dFailedPos[0]);
+    bool test4 = D[dFailedPos[0]] == 0x33 && D[dFailedPos[1]] == 0x77;
 
     cout << "tests: " <<
             test1 << " " <<
             test2 << " " <<
-            test3 << " " << endl;
+            test3 << " " <<
+            test4 << " " << endl;
     return 0;
 }
 
